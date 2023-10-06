08f184dfd9127d12de417bf6b87e6ae663f287b9
Huu <huu@codelight.co>
2023-10-06 16:28:45 +0700

    [COF] ignore

--- build/web/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(async function (message, sender, sendResponse) {
-    console.log("WTF1");
-    console.log(message.data);
-
-      if (message.type === "counter") {
-        sendMessage(message.data);
-      }
-    sendResponse({ title: "document.body.innerHTML" });
-});
